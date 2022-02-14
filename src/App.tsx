40e0511a0aff736d5dfa5d1f7ec8066b1aa8a76e
SnO2WMaN <me@sno2wman.net>
2022-02-15 01:40:50 +0900

    add recoil and auth temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
 import { css } from "@emotion/css";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { RecoilRoot, useSetRecoilState } from "recoil";
+
+import { atomUser } from "./hooks/useAuth/useAuth";
+
+export const Login: React.VFC = () => {
+  const setUser = useSetRecoilState(atomUser);
+
+  return <button onClick={() => setUser({ uid: "01FTD78WNZ2NGCWNTPN59YDKEM" })}>Login</button>;
+};
 
 export const App: React.VFC = () => {
   return (
-    <div
-      className={css(
-        {
-          margin: "64px 64px",
-          /*userSelect: "none" */
-        },
-      )}
-    >
-      <Outlet />
-    </div>
+    <RecoilRoot>
+      <div
+        className={css(
+          {
+            margin: "64px 64px",
+            /*userSelect: "none" */
+          },
+        )}
+      >
+        <nav>
+          <Login />
+        </nav>
+        <Outlet />
+      </div>
+    </RecoilRoot>
   );
 };
--- src/hooks/useAuth/useAuth.ts
+++ src/hooks/useAuth/useAuth.ts
@@ -1,6 +1,17 @@
+import { atom, useRecoilValue } from "recoil";
+
+export const atomUser = atom<
+  | undefined
+  | null
+  | { uid: string; }
+>({ key: "user", default: null });
+
 export const useAuth = ():
-  | { userId: string; }
-  | undefined =>
+  | { loading: true; }
+  | { loading: false; user: null; }
+  | { loading: false; user: { uid: string; }; } =>
 {
-  return { userId: "01FTD78WNZ2NGCWNTPN59YDKEM" };
+  const user = useRecoilValue(atomUser);
+  if (user === undefined) return { loading: true };
+  else return { loading: false, user };
 };
